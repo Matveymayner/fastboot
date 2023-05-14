fd09bcabde59552d8fdc854b8636b310dac2ecaa
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-14 21:50:21 +0300

    eeprom_code.lua

--- eeprom_code.lua
+++ eeprom_code.lua
@@ -1,7 +1,7 @@
 local component = require("component")
 local computer = require("computer")
-local fs = require("filesystem")
- 
+local event = require("event")
+
 -- Функция для загрузки и выполнения файла
 local function runFile(filename)
   local handle = io.open(filename, "r")
@@ -13,27 +13,38 @@ local function runFile(filename)
     print("Ошибка: Файл не найден!")
   end
 end
- 
+
 -- Функция для вывода текста на экран
 local function printCentered(text, y)
   local width, height = component.gpu.getResolution()
   local x = (width - string.len(text)) / 2
   component.gpu.set(x, y, text)
 end
- 
+
 -- Инициализация
 component.gpu.setResolution(80, 25)
 component.gpu.fill(1, 1, 80, 25, " ")
- 
+
 -- Вывод надписи "fastboot"
 printCentered("fastboot", 10)
- 
+
 -- Вывод надписи "by matveymayner"
 printCentered("by matveymayner", 20)
-wait(4) 
+
+-- Ожидание 10 секунд
+local startTime = computer.uptime()
+while computer.uptime() - startTime < 10 do
+  -- Ждем 1 секунду
+  local _, _, _, _, _, currentEvent = event.pull(1)
+  if currentEvent == "interrupted" then
+    return
+  end
+end
+
+-- Запуск файлов
 runFile("OS.lua")
 runFile("HIPOSAV.lua")
 runFile("init.lua")
- 
+
 -- Очистка экрана после выполнения файлов
 component.gpu.fill(1, 1, 80, 25, " ")
