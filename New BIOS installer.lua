48f470a4e41b1acef8304137c1b9eb7b1c4487ab
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-27 10:59:01 +0300

    Binstaller.lua

--- New BIOS installer.lua
+++ New BIOS installer.lua
@@ -1,2 +1,1 @@
-os.execute("wget -q https://raw.githubusercontent.com/Matveymayner/fastboot/main/eeprom_code.lua")
-flash -q eeprom_code.lua FastBoot BIOS
+wget -f https://raw.githubusercontent.com/Matveymayner/fastboot/main/eeprom_code.lua /tmp/eeprom_code.lua && flash -q /tmp/eeprom_code.lua && reboot
